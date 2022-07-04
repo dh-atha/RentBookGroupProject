23eb7eb30e04b217d304d81732caad2f77134716
Atha Dhaiffathin <athadhaiffathin@gmail.com>
2022-07-04 13:52:11 +0700

    database connected

---
+++ entity/users.go
@@ -0,0 +1,8 @@
+package entity
+
+type Users struct {
+	user_id    int
+	user_name  string
+	user_email string
+	password   string
+}
--- main.go
+++ main.go
@@ -2,10 +2,28 @@ package main
 
 import (
 	"RentBookGroupProject/db"
+	"RentBookGroupProject/entity"
 	"fmt"
+	"log"
 )
 
 func main() {
 	conn := db.InitDB()
-	fmt.Println(conn.Ping())
+	// fmt.Println(conn.Ping())
+
+	var usersData entity.Users
+	var daftarUsers = []entity.Users{}
+
+	rows, err := conn.Query("SELECT * FROM users")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for rows.Next() {
+		err := rows.Scan(&usersData)
+		if err != nil {
+			log.Fatal(err)
+		}
+		daftarUsers = append(daftarUsers, usersData)
+	}
+	fmt.Println(daftarUsers)
 }
