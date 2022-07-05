99398975112f1e817fcc197afe83fcbd1f9f6eb8
Atha Dhaiffathin <athadhaiffathin@gmail.com>
2022-07-05 13:30:15 +0700

    create menu awal

--- main.go
+++ main.go
@@ -2,11 +2,42 @@ package main
 
 import (
 	"RentBookGroupProject/db"
-	"log"
+	"fmt"
 )
 
 func main() {
 	conn := db.InitDB()
-	log.Println(conn)
 	db.MigrateDB(conn)
+
+	var inputMenuAwal int
+
+	for inputMenuAwal != 99 {
+		fmt.Print("\n")
+		fmt.Println("1. Register")
+		fmt.Println("2. Login")
+		fmt.Println("3. See Books")
+		fmt.Print("\n")
+		fmt.Print("99. Exit\n\n")
+		fmt.Print("Input: ")
+		fmt.Scanln(&inputMenuAwal)
+
+		switch inputMenuAwal {
+		case 1:
+			Register()
+		case 2:
+			fmt.Println("")
+		case 3:
+			fmt.Println("")
+		case 99:
+			fmt.Println("Exiting program...")
+		default:
+			fmt.Println("Menu tidak terdaftar")
+		}
+	}
 }
+
+func Register() {}
+
+func Login() {}
+
+func SeeBooks() {}
