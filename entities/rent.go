da079fe9e0ec6f89a537cbbe337349fbb75698e3
Atha Dhaiffathin <athadhaiffathin@gmail.com>
2022-07-07 09:25:04 +0700

    ReturnBook Complete

--- entities/rent.go
+++ entities/rent.go
@@ -3,6 +3,7 @@ package entities
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 	"strconv"
 
@@ -17,25 +18,29 @@ type Rent struct {
 
 func BooksRented(db *gorm.DB) {
 	var inputMenu int
-	// var listRents []Rent
-	// result := db.Where("user_id = ?", UserData.ID).Find(&listRents)
-
-	// if result.Error != nil {
-	// 	log.Println("Error occured")
-	// } else {
-	// 	fmt.Println("\n\t---Rented Book List---")
-	// 	for i := 0; i < len(listRents); i++ {
-	// 		fmt.Println("-------------------------------------")
-	// 		fmt.Println("ID\t: ", listRents[i].ID)
-	// 		fmt.Println("Name\t: ", listRents[i].Name)
-	// 		fmt.Println("Type\t: ", listRents[i].Type)
-	// 		if i == len(listRents)-1 {
-	// 			fmt.Println("-------------------------------------")
-	// 		}
-	// 	}
-
-	// }
 	for inputMenu != 99 {
+		var listRents []Rent
+		result := db.Where("user_id = ?", UserData.ID).Find(&listRents)
+
+		if result.Error != nil {
+			log.Println("Error occured")
+		} else {
+			fmt.Println("\n\t---Rented Book List---")
+			for i := 0; i < len(listRents); i++ {
+				bookData := Book{}
+				db.Where("id = ?", listRents[i].BookID).Find(&bookData)
+				fmt.Println("-------------------------------------")
+				fmt.Println("Rent ID\t: ", listRents[i].ID)
+				fmt.Println("Book ID\t: ", listRents[i].BookID)
+				fmt.Println("Book Title\t: ", bookData.Name)
+				fmt.Println("Rent Date\t: ", listRents[i].CreatedAt)
+
+				if i == len(listRents)-1 {
+					fmt.Println("-------------------------------------")
+				}
+			}
+
+		}
 		fmt.Println("\n1. Rent A Book")
 		fmt.Println("2. Return Book")
 		fmt.Println("\n99. Previous")
@@ -90,5 +95,28 @@ func RentBook(db *gorm.DB) {
 }
 
 func ReturnBook(db *gorm.DB) {
-	fmt.Println("Return Book")
+	var rentData Rent
+
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("\n---Return Book---")
+	fmt.Print("RentID: ")
+	scanner.Scan()
+	rentID := scanner.Text()
+
+	res := db.Where("id = ? AND user_id = ?", rentID, UserData.ID).Find(&rentData)
+
+	if res.Error != nil {
+		log.Println("Error Occured")
+	} else {
+		if res.RowsAffected < 1 {
+			fmt.Println("Cant delete that rent data, its either not yours or wrong rentID input")
+		} else {
+			fmt.Println("Successfully returning book")
+			var bookData Book
+			db.Where("id = ?", rentData.BookID).Find(&bookData)
+			bookData.Status = true
+			db.Save(&bookData)
+			db.Delete(&rentData)
+		}
+	}
 }
